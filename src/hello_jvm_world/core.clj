f48be0f88d4173c57d7cd77a8d5023e69983cfbe
tim <llStrainll@gmail.com>
2014-11-14 16:21:20 +0300

    some fixes

--- src/hello_jvm_world/core.clj
+++ src/hello_jvm_world/core.clj
@@ -1,7 +1,9 @@
 (ns hello-jvm-world.core
-  (:gen-class))
+  (:gen-class)
+  (:require hello-jvm-world.horse))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (hello-jvm-world.horse/init 3 3 5))

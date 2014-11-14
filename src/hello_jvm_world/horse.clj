b62efda87d0693b4d91929c9054881ba989ee558
tim <llStrainll@gmail.com>
2014-11-14 15:03:32 +0300

    some fixes

--- src/hello_jvm_world/horse.clj
+++ src/hello_jvm_world/horse.clj
@@ -1,6 +1,32 @@
 (ns hello-jvm-world.horse
-	(:require [hello-jvm-world.permutations :as perm]))
+	(:require [hello-jvm-world.permutations :as perm])
+	(:use [defun :only [defun]]))
 
 (defmacro horse_ways_macro []
-	(perm/make_permutations [-1, -2, 1, 2] 2 (fn [coll] (== (reduce + (map (fn [el] (Math/abs el)) coll)) 3))) )
+	(perm/make_permutations [-1, -2, 1, 2] 2 (fn [coll] (= (reduce + (map #(Math/abs %) coll)) 3))))
 (defn horse_ways [] (horse_ways_macro))
+
+(defrecord Position [x y])
+(defrecord GameState [cur_pos, history])
+
+(defn generate_new_state [old_state way]
+	(->	(update-in old_state [:history]
+			(fn [old_history] (conj old_history (:cur_pos old_state))))
+		(update-in [:cur_pos :x]
+			(fn [x] (+ x (first way))))
+		(update-in [:cur_pos :y]
+			(fn [y] (+ y (last way))))))
+
+(defun game 
+	([_, []] [])
+	([limit, lst]
+		(case (= (dec (* limit limit)) (count (:history (first lst))))
+			true lst
+			false (->>	(map (fn [old_state] 
+							(map #(generate_new_state old_state way) horse_ways))
+						lst)
+						(flatten)
+						(filter is_valid_state)
+						(game limit)
+						(flatten)
+						(vec)))))
--- src/hello_jvm_world/permutations.clj
+++ src/hello_jvm_world/permutations.clj
@@ -2,20 +2,16 @@
 	(:use [defun :only [defun]])
 	(:require clojure.test))
 
-
 (defn make_permutations_process [inp_set take_num condition res]
-		(println res )
-		(cond
-			(== (count res) take_num) 
-				(case (condition res) true res false :failed )
-			(< (count res) take_num) 
-				(map 	(fn [el] (make_permutations_process (filter (fn [elem] (not= elem el)) inp_set) take_num condition (conj res el)))
-						inp_set )))
+	(cond
+		(= (count res) take_num) 
+			(case (condition res) true res false :failed )
+		(< (count res) take_num) 
+			(map (fn [el] (make_permutations_process (filter #(not= % el) inp_set) take_num condition (conj res el))) inp_set )))
 
-(defn flatten_proc [take_num res]
-	(case take_num
-		0 res
-		(flatten_proc (dec take_num) (apply concat res))))
+(defun flatten_proc
+	([0 res] res)
+	([take_num res]	(flatten_proc (dec take_num) (apply concat res))))
 
 (defun make_permutations
 	([	(inp_set :guard #(and (not= [] %) (vector? %)))
@@ -24,5 +20,5 @@
 		(case (<= take_num (count inp_set)) true
 			(->> 	(make_permutations_process inp_set take_num condition [])
 					(flatten_proc (dec take_num))
-					(filter (fn [el] (not= el :failed)))
+					(filter #(not= % :failed))
 					(vec)))))
